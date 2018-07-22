38f34d99ae5b1dc7720d68dfdf826a7780176220
Yoko <18836617@qq.com>
2018-07-22 23:29:21 +0800

    撤销

--- app/web/book.py
+++ app/web/book.py
@@ -1,13 +1,14 @@
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, request
 
-from helper import is_isbn_or_key
-from yushu_book import YuShuBook
+from helper import is_isbn_or_key from yushu_book import YuShuBook
 
 web = Blueprint('web', __name__)
 
 
-@web.route('/book/search/<q>/<page>')
-def search(q, page):
+@web.route('/book/search')
+def search():
+    q = request.args.get('q', '')
+    page = request.args.get('page', '')
     isbn_or_key = is_isbn_or_key(q)
     if isbn_or_key == 'isbn':
         result = YuShuBook.search_by_isbn(q)

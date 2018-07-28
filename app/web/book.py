9f5876819590c6f3468f92e6667877a85652c27b
Yoko <18836617@qq.com>
2018-07-28 10:15:46 +0800

    4-5 WTForms 参数校验

--- app/web/book.py
+++ app/web/book.py
@@ -1,17 +1,23 @@
 from flask import jsonify, Blueprint, request
 
-from helper import is_isbn_or_key from yushu_book import YuShuBook
+from app.forms.book import SearchForm
+from helper import is_isbn_or_key 
+from yushu_book import YuShuBook
 
 web = Blueprint('web', __name__)
 
 
 @web.route('/book/search')
 def search():
-    q = request.args.get('q', '')
-    page = request.args.get('page', '')
-    isbn_or_key = is_isbn_or_key(q)
-    if isbn_or_key == 'isbn':
-        result = YuShuBook.search_by_isbn(q)
+    form = SearchForm(request.args)
+    if form.validate():
+        q = form.q.data.strip()
+        page = form.page.data
+        isbn_or_key = is_isbn_or_key(q)
+        if isbn_or_key == 'isbn':
+            result = YuShuBook.search_by_isbn(q)
+        else:
+            result = YuShuBook.search_by_keyword(q)
+        return jsonify(result)
     else:
-        result = YuShuBook.search_by_keyword(q)
-    return jsonify(result)
+        return jsonify({})

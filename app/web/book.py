7670f9c605997f54dc14f1245a63049f324d8278
Yoko <18836617@qq.com>
2018-07-29 22:01:22 +0800

    7-3 使用ViewModel处理书籍数据下

--- app/web/book.py
+++ app/web/book.py
@@ -3,6 +3,7 @@ from flask import jsonify, Blueprint, request
 from app.forms.book import SearchForm
 from app.libs.helper import is_isbn_or_key 
 from app.spider.yushu_book import YuShuBook
+from app.view_models.book import BookViewModel
 
 web = Blueprint('web', __name__)
 
@@ -16,8 +17,10 @@ def search():
         isbn_or_key = is_isbn_or_key(q)
         if isbn_or_key == 'isbn':
             result = YuShuBook.search_by_isbn(q)
+            result = BookViewModel.packege_single(result, q)
         else:
             result = YuShuBook.search_by_keyword(q, page)
+            result = BookViewModel.packege_collection(result, q)
         return jsonify(result)
     else:
         return jsonify({})

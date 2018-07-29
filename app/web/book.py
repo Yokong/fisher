39ddd2e996a915133d2e47e4d51c3ef62ae0728f
Yoko <18836617@qq.com>
2018-07-29 22:56:56 +0800

    7-7 重构YuShuBook, BookViewModel.  序列化自定义对象

--- app/web/book.py
+++ app/web/book.py
@@ -1,9 +1,11 @@
+import json
+
 from flask import jsonify, Blueprint, request
 
 from app.forms.book import SearchForm
 from app.libs.helper import is_isbn_or_key 
 from app.spider.yushu_book import YuShuBook
-from app.view_models.book import BookViewModel
+from app.view_models.book import BookViewModel, BookCollection
 
 web = Blueprint('web', __name__)
 
@@ -11,16 +13,19 @@ web = Blueprint('web', __name__)
 @web.route('/book/search')
 def search():
     form = SearchForm(request.args)
+    books = BookCollection()
     if form.validate():
         q = form.q.data.strip()
         page = form.page.data
         isbn_or_key = is_isbn_or_key(q)
+        yushu_book = YuShuBook()
         if isbn_or_key == 'isbn':
-            result = YuShuBook.search_by_isbn(q)
-            result = BookViewModel.packege_single(result, q)
+            yushu_book.search_by_isbn(q)
         else:
-            result = YuShuBook.search_by_keyword(q, page)
-            result = BookViewModel.packege_collection(result, q)
-        return jsonify(result)
+            yushu_book.search_by_keyword(q, page)
+        books.fill(yushu_book, q)
+        return json.dumps(books, default=lambda o: o.__dict__), 200, {
+            'content-type': 'application/json'
+        }
     else:
         return jsonify({})

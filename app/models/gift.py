af89bc033aca344cb4ffbf811fc70d4cd3c754dc
Yoko <18836617@qq.com>
2018-08-07 22:42:49 +0800

    11-0 获取最近礼物

--- app/models/gift.py
+++ app/models/gift.py
@@ -1,7 +1,9 @@
-from sqlalchemy import Column, Integer, Boolean, ForeignKey, String
+from flask import current_app
+from sqlalchemy import Column, Integer, Boolean, ForeignKey, String, desc
 from sqlalchemy.orm import relationship
 
 from .base import Base
+from app.spider.yushu_book import YuShuBook
 
 
 class Gift(Base):
@@ -10,3 +12,19 @@ class Gift(Base):
 	uid = Column(Integer, ForeignKey('user.id'))
 	isbn = Column(String(15), nullable=True)
 	launched = Column(Boolean, default=False)
+
+	@property
+	def book(self):
+		yushu = YuShuBook()
+		book = yushu.search_by_isbn(self.isbn)
+		return yushu.first
+
+	
+	@classmethod
+	def recent(cls):
+		recent_gift = Gift.query.filter_by(
+			launched=False).group_by(
+			Gift.isbn).order_by(
+			desc(Gift.create_time)).limit(
+			current_app.config['RECENT_BOOK_COUNT']).distinct().all()
+		return recent_gift
